7843f2e2ea7113e9d1614cc42fd10c9dd2ab37d5
iggy4598 <Iggyjurado45@gmail.com>
2024-10-18 14:23:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const froyo = {
-    vanilla: 3,
-    coffee: 2,
-    strawberry: 1,
-};
+// const froyo = {
+//     vanilla: 3,
+//     coffee: 2,
+//     strawberry: 1,
+// };
 
-console.log(froyo.vanilla)
+// console.log(froyo.vanilla)
+
+const userInputString = prompt("Please select some flavor.", "Vanilla");
+
+const stringArray = userInputString.split(",");
+
+const count = {};
+for (const element of stringArray) {
+  if (count[element]) {
+    count[element] += 1;
+  } else {
+    count[element] = 1;
+  }
+}
+console.log(count);
